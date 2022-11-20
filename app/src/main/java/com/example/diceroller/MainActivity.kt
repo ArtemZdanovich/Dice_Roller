0f796dd790a581bbe9f9c34ed0316ff698d4af98
ArtemZdanovich <gilyen2@mail.ru>
2022-11-20 03:11:53 +0300

    image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -24,10 +25,29 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceRoll1 = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        val resultTextView3: TextView = findViewById(R.id.textView3)
-        resultTextView3.text = diceRoll1.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        when (diceRoll1) {
+            1 -> diceImage2.setImageResource(R.drawable.dice_1)
+            2 -> diceImage2.setImageResource(R.drawable.dice_2)
+            3 -> diceImage2.setImageResource(R.drawable.dice_3)
+            4 -> diceImage2.setImageResource(R.drawable.dice_4)
+            5 -> diceImage2.setImageResource(R.drawable.dice_5)
+            6 -> diceImage2.setImageResource(R.drawable.dice_6)
+        }
+
+
+
     }
 }
 
